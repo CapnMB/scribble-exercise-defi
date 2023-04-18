52ec4d232be5f87a4a83313fc8c4ba7335222dda
CapnMB <30929885+CapnMB@users.noreply.github.com>
2023-04-18 10:23:59 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,35 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
- require("@nomiclabs/hardhat-ethers");
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.6.12",
+solidity: {
+compilers: [
+{
+version: "0.4.21",
+settings: {},
+},
+{
+version: "0.6.0",
+settings: {},
+},
+{
+version: "0.6.12",
+settings: {},
+},
+{
+version: "0.8.0",
+settings: {},
+},
+{
+version: "0.8.1",
+settings: {},
+},
+
+// add more compiler versions here if needed
+],
+},
 
 };
+
