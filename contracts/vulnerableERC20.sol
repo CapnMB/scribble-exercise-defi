93e4ac080841033cb35c6edee03cc039aaf55634
CapnMB <30929885+CapnMB@users.noreply.github.com>
2023-04-18 10:23:23 -0400

    Update vulnerableERC20.sol

--- contracts/vulnerableERC20.sol
+++ contracts/vulnerableERC20.sol
@@ -1,59 +1,83 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-contract VulnerableToken {
-  uint256 private _totalSupply;
-  mapping (address => uint256) private _balances;
-  mapping (address => mapping (address => uint256)) private _allowances;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-  constructor() public {
-    _totalSupply = 1000000;
-    _balances[msg.sender] = 1000000;
-  }
+/// @title A DeFi Smart Contract with mistakes for fuzz testing
+/// @notice This smart contract contains mistakes that can be caught by a fuzz tester
 
-  function totalSupply() external view returns (uint256) {
-    return _totalSupply;
-  }
+contract VulnerableToken is ReentrancyGuard {
+    using SafeERC20 for IERC20;
 
-  function balanceOf(address _owner) external view returns (uint256) {
-    return _balances[_owner];
-  }
+    IERC20 public token;
 
-  function allowance(address _owner, address _spender) external view returns (uint256) {
-    return _allowances[_owner][_spender];
-  }
+    struct DepositInfo {
+        uint256 amount;
+        uint256 depositTimestamp;
+    }
 
-  function transfer(address _to, uint256 _value) external returns (bool) {
-    address from = msg.sender;
-    require(_value <= _balances[from]);
+    mapping(address => DepositInfo) public deposits;
 
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
 
-    uint256 newBalanceFrom = _balances[from] - _value;
-    uint256 newBalanceTo = _balances[_to] + _value;
-    _balances[from] = newBalanceFrom;
-    _balances[_to] = newBalanceTo;
+    /// @param _token Address of the ERC20 token
+    constructor(address _token) {
+        require(_token != address(0), "Invalid token address");
+        token = IERC20(_token);
+    }
 
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
+    /// #invariant forall (address user in deposits) token.balanceOf(address(this)) >= deposits[user].amount;
+    /// #invariant token.balanceOf(address(this)) == sum(address user in deposits, uint256 depositAmount in deposits[user].amount);
 
-  function approve(address _spender, uint256 _value) external returns (bool) {
-    address owner = msg.sender;
-    _allowances[owner][_spender] = _value;
-    emit Approval(owner, _spender, _value);
-    return true;
-  }
+    /// @notice Allows users to deposit tokens
+    /// @param _amount Amount of tokens to deposit
+    function deposit(uint256 _amount) external nonReentrant {
+        require(_amount > 0, "Amount must be greater than 0");
 
-  function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
-    uint256 allowed = _allowances[_from][msg.sender];
-    require(_value <= allowed);
-    require(_value <= _balances[_from]);
-    _balances[_from] -= _value;
-    _balances[_to] += _value;
-    _allowances[_from][msg.sender] -= _value;
-    emit Transfer(_from, _to, _value);
-    return true;
-  }
+        // Mistake: User's previous deposit will be overwritten without withdrawal
+        // #safemath
+        deposits[msg.sender].amount += _amount; // fixed using SafeMath
 
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    /// @notice Allows users to withdraw their tokens
+    /// @param _amount Amount of tokens to withdraw
+    function withdraw(uint256 _amount) external nonReentrant {
+        DepositInfo storage userDeposit = deposits[msg.sender];
+        require(userDeposit.amount >= _amount, "Insufficient balance");
+
+        // Mistake: No time lock for the withdrawal
+        // #timelock
+        require(
+            block.timestamp >= userDeposit.depositTimestamp + 3600,
+            "Withdrawal is not yet available"
+        ); // added time lock
+
+        userDeposit.amount -= _amount;
+
+        token.safeTransfer(msg.sender, _amount);
+
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    /// @notice Calculates interest for the user
+    /// @param _user Address of the user
+    /// @return Interest amount
+    /// @dev This function contains an overflow bug
+    function calculateInterest(address _user) external view returns (uint256) {
+        DepositInfo storage userDeposit = deposits[_user];
+        uint256 depositDuration = block.timestamp -
+            userDeposit.depositTimestamp;
+
+        // Mistake: Overflow bug in interest calculation
+        // #safemath
+        uint256 interest = depositDuration * userDeposit.amount * 1000; // fixed using SafeMath
+
+        return interest;
+    }
 }
